6e5e0387372527c5ad99c2d7face1354801c7930
Mehmet D <exortek979@gmail.com>
2021-07-03 17:40:11 +0300

    1.1.6
    other tables added and some name changes

---
+++ ECommerce.Northwind/Entities/Concrete/Shipper.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Shipper : IEntity
+   {
+        public int ShipperID { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
